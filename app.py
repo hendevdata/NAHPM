d57dfac625f042a355eb63a12f8ad82ee68dc70d
hendevdata <henrylarreal07@gmail.com>
2024-07-18 12:44:39 -0500

    env deleted

--- app.py
+++ app.py
@@ -2,8 +2,7 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import geopandas as gpd
-
+import seaborn as sns
 # Title of the app
 st.title("Survey Data Analysis")
 
